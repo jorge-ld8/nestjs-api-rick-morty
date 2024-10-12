39c993c4b9084a1e09072e05a2d8d30c67feaaa4
jorge-ld8 <jaleon.19@est.ucab.edu.ve>
2024-10-12 17:20:28 -0400

    [FIX]: Adding API Property tags for better API Documentation

--- src/episodes/dto/update-episode.dto.ts
+++ src/episodes/dto/update-episode.dto.ts
@@ -2,7 +2,7 @@ import { PartialType } from '@nestjs/swagger';
 import { CreateEpisodeDto } from './create-episode.dto';
 
 export class UpdateEpisodeDto extends PartialType(CreateEpisodeDto) {
-
+    
     name?: string;
 
     length?: number;
